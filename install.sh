776382076e1762c281f4165150a907ad0a5bd3ce
D3SOX <nico@d3sox.me>
2024-03-14 15:24:47 +0100

    use git as installation method, add update script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 theme_name="Transparent"
 
 # Download URL
-theme_url="https://raw.githubusercontent.com/D3SOX/spicetify-transparent/master"
+theme_url="https://raw.githubusercontent.com/D3SOX/spicetify-transparent"
 
 # Setup directories to download to
 spice_dir="$(dirname "$(spicetify -c)")"
@@ -15,15 +15,13 @@ theme_dir="${spice_dir}/Themes"
 mkdir -p "${theme_dir}/${theme_name}"
 
 # Download latest tagged files into correct director
-echo "Downloading Hazy..."
-curl --silent --output "${theme_dir}/${theme_name}/color.ini" "${theme_url}/color.ini"
-curl --silent --output "${theme_dir}/${theme_name}/user.css" "${theme_url}/user.css"
-curl --silent --output "${theme_dir}/${theme_name}/theme.js" "${theme_url}/theme.js"
+echo "Downloading theme..."
+git clone "$theme_url" "$theme_name"
 echo "Done"
 
 # Apply theme
 echo "Applying theme"
-spicetify config current_theme $theme_name
+spicetify config current_theme "$theme_name"
 spicetify config inject_css 1 replace_colors 1 overwrite_assets 1 inject_theme_js 1
 spicetify apply
 
